c15483c248459ce67095040c3cbbda2e260123d2
RIPO12345 <placida.ripo.naibaho@gmail.com>
2023-03-19 07:38:56 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302213103/Program.cs
+++ tpmodul5_1302213103/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo user " + user);
+    }
+}
+
+public class DataGeneric<T>
+{
+    public T Data { get; set; }
+
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + Data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        string nama = "Ripo";
+        halo.SapaUser(nama);
+
+        DataGeneric<string> data = new DataGeneric<string>("1302213103");
+        data.PrintData();
+    }
+}
